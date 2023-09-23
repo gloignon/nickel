4c9b323b0c8422ba1e8cc033ac0a97742f69c2d2
Guillaume Loignon <guillaume.loignon@gmail.com>
2023-09-23 08:56:59 -0400

    Update nickel.R
    axe

--- nickel.R
+++ nickel.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df <- read_csv("rsqaq_sequentielles_2007-2023.csv", col_types = cols(LD = col_double()))
+df <- read_csv("rsqaq_sequentielles_2007-2023.csv", col_types = cols(LD = col_double()), na = "")
 
 table(df$Contaminant)
 table(df$Station)
@@ -16,6 +16,7 @@ df_nickel_lim %>% ggplot(aes(x = Date, y = Resultat, color = Contaminant)) +
   geom_line() +
   geom_smooth()+
   scale_y_log10()+
+  scale_x_date(date_breaks = "2 year")+
   ggpubr::theme_pubclean() +
   labs(title = "Nickel à Limouilou", caption = "Allo Sol", y = "Résultat (log10)")
 
@@ -24,5 +25,6 @@ df_nickel_lim %>% ggplot(aes(x = Date, y = Resultat, color = Contaminant)) +
   geom_line() +
   geom_smooth()+
   # scale_y_log10()+
+  scale_x_date(date_breaks = "2 years")+
   ggpubr::theme_pubclean() +
   labs(title = "Nickel à Limouilou", caption = "Allo Sol", y = "Résultat")
